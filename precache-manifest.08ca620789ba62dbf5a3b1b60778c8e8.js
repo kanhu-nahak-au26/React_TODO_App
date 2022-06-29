3f66d405dfaea1d0c839ebc414c88eb58462f972
Kanhu <kanhu.kcn@gmail.com>
2022-06-29 23:32:48 +0530

    Updates

---
+++ precache-manifest.08ca620789ba62dbf5a3b1b60778c8e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79209d621c736e4585dc9a80302f90f",
+    "url": "/React_TODO_App/index.html"
+  },
+  {
+    "revision": "8b29003f48e8a69d09a9",
+    "url": "/React_TODO_App/static/css/main.ee5d6995.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/React_TODO_App/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React_TODO_App/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b29003f48e8a69d09a9",
+    "url": "/React_TODO_App/static/js/main.58db7c42.chunk.js"
+  },
+  {
+    "revision": "96bbb5ed7f797a35d94c",
+    "url": "/React_TODO_App/static/js/runtime-main.268a1fcb.js"
+  }
+]);
